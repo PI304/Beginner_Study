2b73274cf98620be2f811ea458e1b56e81d7f7ab
yjc3345 <yjc3345@naver.com>
2023-03-15 15:06:37 +0900

    del_file

--- yjc/mypage/mypage/js/cursor.js
+++
@@ -1,9 +0,0 @@
-//욕심은 크지 항상,,,
-// let cursor = document.querySelector('.cursor');
-
-// window.onload = function () {
-// 	window.addEventListener('mousemove', moveCursor);
-// };
-// function moveCursor(event) {
-// 	cursor.style.transform = `translate(${event.clientX}px,${event.clientY}px)`;
-// }

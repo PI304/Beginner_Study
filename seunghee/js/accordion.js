8bca04e0fd51330849c3af16eeaa76f9205ec3f1
051hui <sheekang30302@gmail.com>
2023-04-25 17:50:03 +0900

    add accordion and stepper

---
+++ seunghee/js/accordion.js
@@ -0,0 +1,13 @@
+const accordion = document.getElementsByClassName("accordion");
+let i;
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}

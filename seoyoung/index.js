cc64058a4e73ed7a20275aa9453b6e0f93349198
seoyoung <127498251+seoyoung1024@users.noreply.github.com>
2023-05-03 16:34:13 +0900

    Week5/tooltip_form
    툴팁, 폼 기능 추가

--- seoyoung/index.js
+++ seoyoung/index.js
@@ -1,11 +1,17 @@
-const button = document.querySelector(".menu1");
+function collapse(element) {
+  var before = document.getElementsByClassName("active")[0]; // 기존에 활성화된 버튼
+  if (before && document.getElementsByClassName("active")[0] != element) {
+    // 자신 이외에 이미 활성화된 버튼이 있으면
+    before.nextElementSibling.style.maxHeight = null; // 기존에 펼쳐진 내용 접고
+    before.classList.remove("active"); // 버튼 비활성화
+  }
+  element.classList.toggle("active"); // 활성화 여부 toggle
 
-button.addEventListener("click", () => {
-  const dropdown = document.querySelector(".dropdownmenu");
-  dropdown.style.display = "block";
-});
-
-button.addEventListener("blur", () => {
-  const dropdown = document.querySelector(".dropdownmenu");
-  dropdown.style.display = "none";
-});
+  var content = element.nextElementSibling;
+  if (content.style.maxHeight != 0) {
+    // 버튼 다음 요소가 펼쳐져 있으면
+    content.style.maxHeight = null; // 접기
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px"; // 접혀있는 경우 펼치기
+  }
+}

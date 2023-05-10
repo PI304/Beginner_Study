6f693ffe435e290b29ac6e08ed84a0a57666745a
Hui Kang <sheekang30302@gmail.com>
2023-05-10 16:32:11 +0900

    Week6/carouselscroll (#31)
    * add modal, dropdown
    
    * accordion rating_textbox update
    
    * add carousel

--- seunghee/js/accordion.js
+++ seunghee/js/accordion.js
@@ -2,12 +2,19 @@ const accordion = document.getElementsByClassName("accordion");
 let i;
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+    let j;
+    for (j = 0; j < accordion.length; j++) {
+      const panels = accordion[j].nextElementSibling;
+      panels.style.maxHeight = null;
+      accordion[j].classList.remove("active");
+    }
     const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
+      this.classList.remove("active");
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
+      this.classList.add("active");
     }
   });
 }

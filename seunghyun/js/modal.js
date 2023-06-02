2d7d2d778d4e810cbfaf20a16508d7345a243b18
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-06-02 20:51:51 +0900

    Week9/apiupdate (#39)
    * Feat: api update

---
+++ seunghyun/js/modal.js
@@ -0,0 +1,33 @@
+
+// 모달창
+window.onload = function () {
+
+    function onClick() {
+        document
+            .querySelector('.modal_wrap')
+            .style
+            .display = 'block';
+        document
+            .querySelector('.black_bg')
+            .style
+            .display = 'block';
+    }
+    function offClick() {
+        document
+            .querySelector('.modal_wrap')
+            .style
+            .display = 'none';
+        document
+            .querySelector('.black_bg')
+            .style
+            .display = 'none';
+    }
+
+    document
+        .querySelector('modal_btn')
+        .addEventListener('click', onClick);
+    document
+        .querySelector('.modal_close')
+        .addEventListener('click', offClick);
+
+};

580324c855e89c078314b4c3470ff13fee4983c0
seoyoung <127498251+seoyoung1024@users.noreply.github.com>
2023-06-28 13:47:45 +0900

    Week8/api (#38)
    * 툴팁, 폼 기능 추가
    
    * api수정
    
    ---------
    
    Co-authored-by: seoyoung <jjssyy3614@gmail.com>

--- seoyoung/api.js
+++ seoyoung/api.js
@@ -1,17 +1,29 @@
 const getData = async () => {
   const res = await axios
-  .get('http://52.78.238.141/api/users')
-  .catch((error) => console.log(error));
+    .get("http://52.78.238.141/api/users")
+    .catch((error) => console.log(error));
   const users = res.data;
   console.log(users);
-}
-getData();
+};
 
-const postData = async () => {
-  const res = await axios
-  .post('http://52.78.238.141/api/posts/content')
-  .catch((error) => console.log(error));
-  const users = res.data;
-  console.log(users);
+function addUser() {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const url = document.getElementById("url");
+  const introduction = document.getElementById("message");
+  const data = {
+    name: name.value,
+    email: email.value,
+    url: url.value,
+    introduction: introduction.value,
+  };
+
+  axios
+    .post("http://52.78.238.141/api/users", data)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
-postData();
